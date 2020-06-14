46214e19cb3685c94b6d74493904176e253a4357
Joseph Perez <hello@josephdanielperez.com>
2020-06-14 23:59:45 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -23,16 +23,30 @@ def valid_move?(board, index)
   index.between?(0,8) && !position_taken?(board, index)
 end
 
+def current_player(board)
+  turn_count(board) % 2 == 0? "X" : "O"
+end
+
 def turn(board)
   puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
+  user_input = gets.strip
+  index = input_to_index(user_input)
   if valid_move?(board, index)
-    move(board, index)
+    player_move(board, index, current_player(board))
     display_board(board)
   else
     turn(board)
   end
+end
+
+def turn_count(board)
+  counter = 0
+  board.each {|space|
+    if space == "X" || space == "O"
+      counter += 1   
+    end
+  }
+  counter
 end
 
 def play(board)
